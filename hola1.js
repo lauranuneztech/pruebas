dc73f367c73869ccfab64096b202f8e6e25da317
lauranuneztech <lauranunez1412@gmail.com>
2022-08-08 19:10:40 -0300

    mi primer commit

---
+++ hola1.js
@@ -0,0 +1,37 @@
+// //hola mundo digital house
+// let saludo = "hola mundo  digital house 2"
+// let nombre = "facu"
+// let apellido = "erbin"
+// console.log ("10" + 10 +" " + nombre + " " + apellido)
+
+// //console.log (2+3*5)
+
+// function sumar (numeroA, numeroB){
+//  return numeroA + numeroB;   
+// }
+
+// let sumarAYB= sumar (100,30);
+// //console.log (sumarAYB);
+
+
+// let sumaCD= sumar(90,110);
+// //console.log (sumaCD)
+
+// function saludar (nombre, apellido){
+//     return 'hola' + " " + nombre + apellido;
+// }
+// console.log (saludar ('manuel',' ','pereira'));
+
+let array1 = ['perro','gato','lagarto','pajaro'];
+
+console.log(array1 [0]);
+
+let arbol = [];
+
+console.log(arbol[0]);
+
+let mecedora = ["motor", "enchufe"];
+
+console.log(mecedora[48]);
+
+
